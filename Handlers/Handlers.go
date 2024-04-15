857357820642424b7915409fd1cad88a527df834
Varnit Sharma <94976653+Varnit01-dev@users.noreply.github.com>
2024-04-15 23:23:24 +0530

    Update Handlers.go

--- Handlers/Handlers.go
+++ Handlers/Handlers.go
@@ -13,11 +13,11 @@ func (h *Login) Handle(c echo.Context) error {
 	ctx := c.Request().Context()
 	if ctx == nil {
 		ctx = context.Background()
-	} // request handling logic
+	} 
 }
 
 func chainMiddleware(route Route) []echo.MiddlewareFunc {
-	// middleware chining logic
+	
 }
 
 func (h *Login) buildErrorResponse(ctx context.Context, response *pb.LoginResponse, c echo.Context, errorCode codes.Code, message string) (*util.Response, error) {
